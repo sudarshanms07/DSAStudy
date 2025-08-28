3c9f6c9894cadc888e445338d274efc209507ef6
Sudarshan Maske <ms22maske@gmail.com>
2025-08-28 17:57:13 +0530

    searching in BST

--- BinaryTree/BST/createAndInsert.cpp
+++ BinaryTree/BST/createAndInsert.cpp
@@ -72,9 +72,87 @@ void levelOrderTraversal(Node* root){
     }
 }
 
+Node* minValue(Node* root) {
+	if(root == NULL) {
+		cout << "NO MIN VALUE" << endl;
+		return NULL; 
+	}
+	Node* temp = root;
+
+	while(temp->left != NULL) {
+		temp = temp->left;
+	}
+	return temp;
+}
+
+Node* maxValue(Node* root) {
+	if(root == NULL) {
+		return NULL;
+	}
+
+	//if tree has nodes > 0 count
+	Node* temp = root;
+	while(temp->right != NULL) {
+		temp = temp->right;
+	}
+	return temp;
+}
+
+bool searchBST(Node* root,int target){
+    if(root == NULL){
+        return false;
+    }
+    if(target == root->data){
+        return true;
+    }
+
+    bool leftAns= false;
+    bool rightAns= false;
+
+    if(target > root->data){
+        rightAns = searchBST(root->right,target);
+    }
+    else{
+        leftAns = searchBST(root->left, target);
+    }
+
+    return leftAns || rightAns;
+}
+
+
 int main(){
     Node* root = NULL;
 	createBST(root);
 
 	levelOrderTraversal(root);
+
+    	Node* minNode = minValue(root);
+	if(minNode == NULL) {
+		cout << "There is no node in tree, so no min value" << endl;
+	}
+	else {
+		cout << "Min Value: " << minNode->data << endl;
+	}
+
+	cout << endl;
+	Node* maxNode = maxValue(root);
+	if(maxNode == NULL) {
+		cout << "There is no node in tree, so no max value" << endl;
+	}
+	else {
+		cout << "Max Value: " << maxNode->data << endl;
+	}
+
+    int t;
+	cout << "Enter the target: " << endl;
+	cin >> t;
+
+	while(t != -1) {
+		bool ans = searchBST(root, t);
+		if(ans == true) {
+			cout << "Found" << endl;
+		}
+		else {
+			cout << "Not Found" << endl;
+		}
 }
